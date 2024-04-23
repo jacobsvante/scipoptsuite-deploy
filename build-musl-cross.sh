f555fa3c187b88ec5029e50a4cf3dbe8131c7447
Jacob Magnusson <m@jacobian.se>
2024-04-23 17:07:09 +0200

    wip

--- build-musl-cross.sh
+++ build-musl-cross.sh
@@ -1,4 +1,6 @@
 #/usr/bin/env sh
+# Script that builds Dockerfile.musl-cross and records the file and Docker' output.
+# Useful when trying to optimize the build, to see which flags worked and which didn't.
 
 set -e
 
@@ -12,3 +14,9 @@ md5 -q $DOCKERFILE > $DIRNAME/$DOCKERFILE.md5
 echo "$@" > $DIRNAME/extra-args-passed.txt
 docker buildx build --progress=plain -f $DOCKERFILE -t $DOCKER_IMAGE_NAME "$@" . 2>&1 \
     | tee $DIRNAME/build.log
+docker run --rm $DOCKER_IMAGE_NAME ls -ahl scip_install/bin > $DIRNAME/ls-ahl-output.txt
+docker run --rm $DOCKER_IMAGE_NAME ls -ahl scip_install/lib >> $DIRNAME/ls-ahl-output.txt
+docker run --rm $DOCKER_IMAGE_NAME ldd scip_install/bin/soplex > $DIRNAME/ldd-output.txt
+docker run --rm $DOCKER_IMAGE_NAME ldd scip_install/bin/scip >> $DIRNAME/ldd-output.txt
+docker run --rm $DOCKER_IMAGE_NAME file scip_install/bin/soplex > $DIRNAME/file-output.txt
+docker run --rm $DOCKER_IMAGE_NAME file scip_install/bin/scip >> $DIRNAME/file-output.txt
